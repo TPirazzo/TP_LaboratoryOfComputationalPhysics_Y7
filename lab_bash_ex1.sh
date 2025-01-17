1253103be2a7da92630a9c3648a2483d2017d79b
Tommaso Pirazzo <tommaso.pirazzo@proton.me>
2025-01-17 09:44:06 +0100

    lesson 05 ok

--- lab_bash_ex1.sh
+++ lab_bash_ex1.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#Make a new directory called "students" in your home
-mkdir $HOME/students
-
-#Download a csv file with the list of students of this lab and copy that to students
+#Make a new directory called "students" in your home and get in
+#mkdir $HOME/students
 cd $HOME/students
-wget -O LCP_22-23_students.csv "https://www.dropbox.com/scl/fi/bxv17nrbrl83vw6qrkiu9/LCP_22-23_students.csv?rlkey=47fakvatrtif3q3qw4q97p5b7&e=1"
 
 #Check whether the file is already there
-if [ -f "/home/tommaso/students/LCP_22-23_students.csv" ]; then
-  echo "File exists"
+if [ -e "/home/pira/students/LCP_22-23_students.csv" ]; then
+  echo "The file already exists"
 else
-  echo "File does not exist"
+  echo "The file doesn't exist. Let's to download it!"
+  #Download a csv file with the list of students of this lab and copy that to students
+  wget -O LCP_22-23_students.csv "https://www.dropbox.com/scl/fi/bxv17nrbrl83vw6qrkiu9/LCP_22-23_students.csv?rlkey=47fakvatrtif3q3qw4q97p5b7&e=1"
 fi
 
 #Make two new files, one containing the students belonging to PoD, the other to Physics.
@@ -23,7 +22,7 @@ count_list=()
 letter_list=()
 
 for letter in {A..Z}; do
-  count=$(grep -c "^$letter" "/home/tommaso/students/LCP_22-23_students.csv")
+  count=$(grep -c "^$letter" "/home/pira/students/LCP_22-23_students.csv")
   count_list+=($count)  #store the counts in a list
   letter_list+=($letter)  #store the letters in a list
   echo "$letter: $count"
@@ -37,24 +36,14 @@ for ((i=1; i<=${#count_list[@]}; i++)); do
   fi
 done
 
+#Print out the mostrecurrent surname's initial letter
 echo "The most recurrent letter is: $max_l"
 
 #Assume an obvious numbering of the students in the file (first line is 1,
 #second line is 2, etc.), group students "modulo 18", i.e. 1,19,37,.. 2,20,38,.. etc.
 #and put each group in a separate file
+# First eliminate the first line of the file
+sed -i '1d' LCP_22-23_students.csv
 for (( i=0; i<=17; i++ )); do
   awk "NR%18==$i" "LCP_22-23_students.csv" > modulo18_group_$i.txt
 done
-#done
-#line_number=$(wc -l LCP_22-23_students.csv | cut -d' ' -f1)
-#line_number=$(($line_number - 1))
-#echo "$line_number"
-#rest=$(($line_number % 18))
-#echo "rest: $rest"
-
-#for ((i=1; i<=4; i++)); do
-#  while read line; do
-
-#while read line; do
-#  if
-#done
